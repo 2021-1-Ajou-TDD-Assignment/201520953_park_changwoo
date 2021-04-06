4d166fe03891805fee0680180afa72d5f566ddd8
Parkchangwoo <Parkchangwoo@LAPTOP-CHS7USMU>
2021-04-06 16:50:39 +0900

    add: game.java feat: testGutterGame()

--- test/BowlingGameTest.java
+++ test/BowlingGameTest.java
@@ -5,8 +5,15 @@ import org.junit.Test;
 public class BowlingGameTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testGutterGame() {
+		
+		Game g = new Game();
+		
+		for(int i = 0; i < 20; i++) {
+			
+			g.roll(0);
+		}
+		assertEquals(0, g.score());
 	}
 
 }
